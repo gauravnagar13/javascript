9a753446e3af7a0d0befb05f3405b6547b1c76c4
Gaurav Nagar <98938050+gauravnagar13@users.noreply.github.com>
2023-10-17 16:06:40 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 235
+let accountEmail = "nagargorv@gmail.com"
+var accountPassword = "12345"
+accountcity = "kota"
+console.table([accountId, accountEmail, accountPassword,accountcity]);
